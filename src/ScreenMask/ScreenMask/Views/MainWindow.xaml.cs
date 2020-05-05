0041d832fe2bcbc1fe13c0659af8df8de24c2419
kusa-mochi <whoatemyapplepie@gmail.com>
2020-05-05 13:23:12 +0900

    make basic layout on floating menu.

--- src/ScreenMask/ScreenMask/Views/MainWindow.xaml.cs
+++ src/ScreenMask/ScreenMask/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace ScreenMask.Views
 {
@@ -10,6 +13,32 @@ namespace ScreenMask.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void EditorMenuThumb_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+
+        }
+
+        private void EditorMenuThumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            Thumb thumb = sender as Thumb;
+            if (thumb == null) return;
+
+            double x = Canvas.GetLeft(EditorMenu) + e.HorizontalChange;
+            double y = Canvas.GetTop(EditorMenu) + e.VerticalChange;
+
+            x = Math.Max(x, 0);
+            y = Math.Max(y, 0);
+            x = Math.Min(x, EditorMenuDragArea.ActualWidth - EditorMenu.ActualWidth);
+            y = Math.Min(y, EditorMenuDragArea.ActualHeight - EditorMenu.ActualHeight);
+
+            Canvas.SetLeft(EditorMenu, x);
+            Canvas.SetTop(EditorMenu, y);
+        }
+
+        private void EditorMenuThumb_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
         }
     }
 }
